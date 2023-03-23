c65669928eef149422de339d449f4ca8f60801bb
Sébastien Mantanus <sebastienmantanus@MBPdeSebastien.numericable.fr>
2023-03-23 18:05:13 +0100

    SignUp confirmation email done

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,13 +1,60 @@
+import axios from "axios";
 import { useState } from "react";
+import Cookies from "js-cookie";
 
 const SignupForm = ({ SetToken }) => {
   const [name, SetName] = useState("");
   const [email, SetEmail] = useState("");
   const [password, SetPassword] = useState("");
   const [passwordConfirmation, SetPasswordConfirmation] = useState("");
+  const [error, SetError] = useState("");
 
-  const handleSubmit = (event) => {
+  const message = "Ceci est un test";
+
+  const confirmationEmail = async () => {
+    try {
+      const response = await axios.post(
+        "https://mails--sendinblue--pcsmmwq8bwzd.code.run/connectify",
+        {
+          responder_name: name,
+          responder_email: email,
+          custom_message: message,
+        }
+      );
+      return console.log(response.data);
+    } catch (error) {
+      console.log(error.data);
+    }
+  };
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
+
+    if (name && email && password && passwordConfirmation) {
+      if (password === passwordConfirmation) {
+        const response = await axios.post(
+          "http://localhost:3000/users/create",
+          {
+            name: name,
+            email: email,
+            password: password,
+          }
+        );
+        if (response.data.token) {
+          confirmationEmail();
+          let name = JSON.stringify(response.data.name);
+          Cookies.set("name", name);
+          Cookies.set("token", response.data.token);
+          SetToken(response.data.token);
+        } else {
+          SetError("Quelque chose ne va pas...");
+        }
+      } else {
+        SetError("Mots de passe différents");
+      }
+    } else {
+      SetError("Merci de renseigner tous les champs");
+    }
   };
   return (
     <div>
@@ -46,6 +93,7 @@ const SignupForm = ({ SetToken }) => {
           }}
         />
         <button>Créer mon compte</button>
+        {error && <p className="error-message">{error}</p>}
       </form>
     </div>
   );
