2afde99518201eb4a7c0b3b463b3685465ab92ca
Sébastien Mantanus <sebastienmantanus@iMac-de-Sebastien.local>
2023-03-24 19:23:27 +0100

    home page in progress

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,85 @@
-import { useNavigate, Navigate } from "react-router-dom";
+import { Navigate, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Home = ({ token, SetToken }) => {
-  const navigate = useNavigate();
+  const [isLoading, SetIsLoading] = useState(true);
+  const [data, SetData] = useState();
+  const [user, SetUser] = useState();
+  const [search, SetSearch] = useState("");
+  const [searchType, SetSearchType] = useState("name");
+
+  let filter = "";
+  if (search.length > 3) {
+    filter = `&${searchType}=${search}`;
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        `http://localhost:3000/affiliates-search?limit=12+${filter}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      SetData(response.data[1]);
+      SetUser(response.data[0]);
+      SetIsLoading(false);
+    };
+    fetchData();
+  }, [search, filter, token]);
+
   return token ? (
-    <div>
-      <h1>Home Page, Welcome {token}</h1>
-      <button
-        onClick={() => {
-          SetToken("");
-          navigate("/");
-        }}
-      >
-        Se déconnecter
-      </button>
-    </div>
+    isLoading ? (
+      <div>Loading in progress</div>
+    ) : (
+      <div className="flex-center font">
+        <div
+          className="home-container border-red
+  "
+        >
+          <div className="top-bar">
+            <div>{data.length} contacts</div>
+            <div>
+              <select
+                onChange={(event) => {
+                  SetSearchType(event.target.value);
+                }}
+              >
+                <option value="name">Entreprise</option>
+                <option value="contact">Contact</option>
+                <option value="email">Email</option>
+              </select>
+              <input
+                type="text"
+                placeholder="Rechercher..."
+                value={search}
+                onChange={(event) => {
+                  SetSearch(event.target.value);
+                }}
+              />
+            </div>
+            <div>{searchType}</div>
+          </div>
+
+          <div className="contact-grid">
+            {data.map((item, index) => {
+              return (
+                <div key={item._id} className="contact-cards">
+                  <h2>{item.name}</h2>
+                  <h3>{item.contact}</h3>
+                  <p>{item.description}</p>
+                  <p>{item.telephone}</p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    )
   ) : (
     <Navigate to="/" />
   );
