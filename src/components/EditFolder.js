a37efe967d24268a583f2ab9308d924df6d20fd8
Sébastien Mantanus <sebastienmantanus@MacBook-Pro-de-Sebastien.local>
2023-06-15 10:49:56 +0200

    affiliates list in folders with avatar ok

--- src/components/EditFolder.js
+++ src/components/EditFolder.js
@@ -1,12 +1,18 @@
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const EditFolder = ({ token, server, folderId }) => {
   const [newFolderName, setNewFolderName] = useState("");
   const [newFolderDescription, setNewFolderDescription] = useState("");
-  const [nbAffiliates, setNbAffiliates] = useState(0);
+  const [nbAffiliates, setNbAffiliates] = useState("");
   const [responsable, setResponsable] = useState("");
   const [isLoading, setIsLoading] = useState(true);
+
+  // set the overIndex
+  const [hoveredIndex, setHoveredIndex] = useState(null);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     // fetching forder
@@ -50,6 +56,19 @@ const EditFolder = ({ token, server, folderId }) => {
     }
   };
 
+  // number of affiliates in the folder switch
+
+  const nbAffiliatesSwitch = () => {
+    switch (nbAffiliates.length) {
+      case 0:
+        return <p>Aucun contact dans ce dossier</p>;
+      case 1:
+        return <p>1 contact dans ce dossier</p>;
+      default:
+        return <p>{nbAffiliates.length} contacts</p>;
+    }
+  };
+
   return isLoading ? (
     <div>Chargement en cours...</div>
   ) : (
@@ -78,14 +97,35 @@ const EditFolder = ({ token, server, folderId }) => {
       <div>
         <div>
           {nbAffiliates &&
-            nbAffiliates.map((affiliate, index) => {
+            nbAffiliates.map((affiliate) => {
               return (
-                <div key={index} id="favicons">
-                  <p>{affiliate.company_name}</p>
+                <div
+                  // set the hoveredIndex on mouse over
+                  onMouseOver={() => {
+                    setHoveredIndex(affiliate._id);
+                  }}
+                  onMouseOut={() => {
+                    setHoveredIndex(null);
+                  }}
+                  onClick={() => {
+                    navigate(`/contact/${affiliate._id}/edit`);
+                  }}
+                >
+                  <img
+                    key={affiliate._id}
+                    src={affiliate.company_favicon.url}
+                    alt="favicon"
+                    title={affiliate.company_name}
+                  />
+                  {hoveredIndex === affiliate._id && (
+                    <p>{affiliate.company_name}</p>
+                  )}
                 </div>
               );
             })}
         </div>
+
+        {nbAffiliatesSwitch()}
         <p>Créé par : {responsable}</p>
       </div>
     </>
